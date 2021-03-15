e5bd2410c2e6a9055c96c62b5b0f3f39a89bcbfe
hostedposted <hostedpostedsite@gmail.com>
2021-03-15 11:29:11 -0400

    Make error have red text.

--- ArenaPointGenerator.py
+++ ArenaPointGenerator.py
@@ -6,7 +6,7 @@ password = input("What is your password?\n")
 try:
     token = json.loads(subprocess.Popen(f"node tokenify.js {username} {password}", shell=True, stdout=subprocess.PIPE).stdout.read().decode())
 except:
-    print("Username or Password are incorrect.")
+    print("\x1b[31mUsername or Password are incorrect.\x1b[0m")
     exit()
 userID = token["userID"]
 token = f'Bearer {token["token"]}'
